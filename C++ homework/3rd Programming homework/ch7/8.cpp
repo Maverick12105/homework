996631207288456752d81dd983f4fd01a3f4fa89
Maverick <mike940613@gmail.com>
2024-04-29 00:43:00 +0800

    Fix: forgot to delete something

--- C++ homework/3rd Programming homework/ch7/8.cpp
+++ C++ homework/3rd Programming homework/ch7/8.cpp
@@ -1,16 +1,5 @@
-/*
-	Write a program that outputs a histogram of grades for an assignment given to a class of students.
-	The program should input each student's grade as an integer and store the grade in a vector.
-	Grades should be entered until the user enters -1 for a grade.
-	The program should then scan through the vector and compute the histogram.
-	In computing the histogram, the minimum value of a grade is 0, but your program should determine the maximum value entered by the user.
-	Output the histogram to the console.
-	See Programming Project 5.7 for information on how to compute a histogram.
-*/
-
 #include <iostream>
 #include <vector>
-#include <math.h>
 using namespace std;
 
 int main()
